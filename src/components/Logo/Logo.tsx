f70a0441cbf9472fdcd0114d0635750c2fc4a386
LeontevV <v.leontev@dunice.net>
2023-02-03 11:47:39 +0300

    logoText

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -7,7 +7,7 @@ import './Logo.css';
 function Logo() {
   return (
     <div className='containerHeader'>
-      <img className='logo' src={Image} alt='' />
+      <div className='logo'>SEMIOTECH</div>
       <div />
     </div>
   );
