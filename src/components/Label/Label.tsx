78987a6c478c6196fb8e394a3acbfe8619cdf2c1
LeontevV <v.leontev@dunice.net>
2023-02-06 10:19:45 +0300

    delete inline

--- src/components/Label/Label.tsx
+++ src/components/Label/Label.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import loginBackGround from '../../assets/svg/loginBackGround.svg';
 
 import style from './Label.module.scss';
 
 function Label() {
   return (
     <div className={style.labelText}>
-      <div className={style.labelText} style={{ backgroundImage: `url(${loginBackGround})` }}>
-        Sign up
-      </div>
+      <div className={style.labelText}>Sign up</div>
     </div>
   );
 }
