836b2b4afe1b2f6d33b229333ebae62b9a69e55e
LeontevV <v.leontev@dunice.net>
2023-02-06 10:38:05 +0300

    font-face

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-import '@fontsource/work-sans';
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
