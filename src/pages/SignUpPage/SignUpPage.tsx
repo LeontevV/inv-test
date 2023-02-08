ea90186eac432e8453de6014cb8e0ac183a06585
LeontevV <v.leontev@dunice.net>
2023-02-08 14:34:19 +0300

    clear style

--- src/pages/SignUpPage/SignUpPage.tsx
+++ src/pages/SignUpPage/SignUpPage.tsx
@@ -1,4 +1,5 @@
 import React, { memo } from 'react';
+import Button from '../../components/core/Button/Button';
 import Label from '../../components/Label/Label';
 
 import style from './SignUpPage.module.scss';
@@ -7,6 +8,7 @@ function SignUpPage() {
   return (
     <div className={style.containerSignUp}>
       <Label />
+      <Button title='create account' />
     </div>
   );
 }
