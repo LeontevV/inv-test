506092ce495e49b514066b3e3f985686b4a21e05
LeontevV <v.leontev@dunice.net>
2023-02-17 11:54:39 +0300

    get 201 create

--- src/utils/requestAuth.ts
+++ src/utils/requestAuth.ts
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Navigate } from 'react-router-dom';
 import instance from '../api/axios';
 
 type AuthFormProps = {
@@ -13,6 +14,12 @@ const requestAuth = async (path: string, authForm: AuthFormProps) => {
   try {
     const response = await instance.post(path, authForm);
     const accessToken = response?.data;
+    console.log('response.status', response.status);
+
+    if (response.status === 201) {
+      return <Navigate to={'/login'} />;
+    }
+    console.log('response', response);
   } catch (err) {
     alert(err);
   }
